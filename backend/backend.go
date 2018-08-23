faa27256413b5fe72a337a900c348e6eb54a5287
Andrew Cuga <andycuga@gmail.com>
2018-08-23 07:56:59 -0400

    First attempt at backend rest service

--- backend/backend.go
+++ backend/backend.go
@@ -1,14 +1,38 @@
 package backend
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"github.com/TheAndruu/git-leaderboard/models"
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/log"
 )
 
 func init() {
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/repostats", saveRepoPost)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Hello cruel, cruel world!")
+}
+
+func saveRepoPost(w http.ResponseWriter, r *http.Request) {
+	ctx := appengine.NewContext(r)
+	log.Infof(ctx, "Saving repo stats")
+	target := models.RepoStats{}
+	// if err != nil {
+	// 	log.Errorf(ctx, "Error doing http request: %v", err)
+	// 	return nil, err
+	// }
+	json.NewDecoder(r.Body).Decode(&target)
+
+	// fmt.Fprint(w, "Hello cruel, cruel world!")
+
+	// Write content-type, statuscode, payload
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(201)
+	fmt.Fprintf(w, "%s", target.RepoName)
 }
