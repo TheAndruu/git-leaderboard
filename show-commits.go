9dff0cd7630e3699102528ffeef530e28c083d9f
Andrew Cuga <andycuga@gmail.com>
2018-08-22 22:11:03 -0400

    Adding instructions for sending params

--- show-commits.go
+++ show-commits.go
@@ -13,15 +13,29 @@ import (
 )
 
 func main() {
-	commitCounts := getRepoCommits()
-	pretty.Print(commitCounts)
+	repoStats := initStatsFromArgs()
+	repoStats.Commits = getRepoCommits()
+	pretty.Print(repoStats)
+}
+
+func initStatsFromArgs() models.RepoStats {
+	args := os.Args[1:]
+	if len(args) != 2 {
+		fmt.Fprintln(os.Stderr, "Please enter 2 parameters: the project name and its URL, for example:")
+		fmt.Fprintln(os.Stderr, "")
+		fmt.Fprintln(os.Stderr, "    git-leaderboard \"golongpoll\" \"https://github.com/jcuga/golongpoll\"")
+		fmt.Fprintln(os.Stderr, "")
+		os.Exit(1)
+	}
+	stats := models.RepoStats{RepoName: args[0], RepoURL: args[1]}
+	return stats
 }
 
 func getRepoCommits() []models.CommitCount {
 	cmdOut, err := exec.Command("git", "shortlog", "master", "--summary", "--numbered").Output()
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "There was an error running the git command: ", err)
-		os.Exit(1)
+		os.Exit(2)
 	}
 
 	commitLines := strings.Split(string(cmdOut), "\n")
@@ -38,7 +52,7 @@ func getRepoCommits() []models.CommitCount {
 		numCommits, err := strconv.Atoi(strings.TrimSpace(commitLine[0]))
 		if err != nil {
 			fmt.Fprintln(os.Stderr, "Trouble reading the number of commits for author: ", author, err)
-			os.Exit(2)
+			os.Exit(3)
 		}
 		authorCommit := models.CommitCount{Author: author, NumCommits: numCommits}
 		commitCounts = append(commitCounts, authorCommit)
