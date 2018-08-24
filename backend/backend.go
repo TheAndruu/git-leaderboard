22e65ee6767468da19bf08ab3afb2fc247694cff
Andrew Cuga <andycuga@gmail.com>
2018-08-23 23:23:54 -0400

    Deleting redundant code lines

--- backend/backend.go
+++ backend/backend.go
@@ -23,21 +23,16 @@ func saveRepoPost(w http.ResponseWriter, r *http.Request) {
 	ctx := appengine.NewContext(r)
 	log.Infof(ctx, "Saving repo stats")
 	target := models.RepoStats{}
-	// if err != nil {
-	// 	log.Errorf(ctx, "Error doing http request: %v", err)
-	// 	return nil, err
-	// }
 	json.NewDecoder(r.Body).Decode(&target)
-
-	// fmt.Fprint(w, "Hello cruel, cruel world!")
 
 	// Write content-type, statuscode, payload
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(201)
-	log.Infof(ctx, fmt.Sprintf("Value: %v", target.RepoName))
+	log.Infof(ctx, fmt.Sprintf("Accepting repo name: %v", target.RepoName))
+
 	reMarshalled, err := json.Marshal(target)
 	if err != nil {
 		log.Errorf(ctx, "Issue marshalling json to string %v", err)
 	}
-	fmt.Fprintf(w, "Got to %v", fmt.Sprintf("%v\n", string(reMarshalled)))
+	fmt.Fprintf(w, "%v", string(reMarshalled))
 }
