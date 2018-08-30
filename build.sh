fd8cd751fecd74e331eb827124fd511d3276bc25
Andrew Cuga <andycuga@gmail.com>
2018-08-29 21:19:53 -0400

    Adding different arches

--- build.sh
+++ build.sh
@@ -5,7 +5,11 @@ GOARCH=amd64
 
 # Build windows
 GOOS=windows
-go build -v -o build/show-commits.exe
+go build -v -o build/show-commits-x64.exe
+
+GOARCH=386
+go build -v -o build/show-commits-x32.exe
+
 
 # Build mac
 GOOS=darwin
