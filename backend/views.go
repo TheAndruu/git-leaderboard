004665a196eac8d51a8544a48c3afb414acf7d37
Andrew Cuga <andycuga@gmail.com>
2018-09-01 11:11:46 -0400

    Removing welcome view

--- backend/views.go
+++ backend/views.go
@@ -7,20 +7,6 @@ import (
 	"google.golang.org/appengine/log"
 )
 
-// A Welcome message with title, demonstrates passing data to a template
-type Welcome struct {
-	Title   string
-	Message string
-}
-
-// A template taking a struct pointer (&message) containing data to render
-func welcomeHandler(w http.ResponseWriter, r *http.Request) {
-	message := Welcome{Title: "Bootstrap, Go, and GAE", Message: "Bootstrap added to Golang on App Engine.  Feel free to customize further"}
-
-	// outerTheme refernces the template defined within theme.html
-	templates["welcome.html"].ExecuteTemplate(w, "outerTheme", &message)
-}
-
 // Shows the leaders in the current git repos
 func showLeaders(w http.ResponseWriter, r *http.Request) {
 	ctx := appengine.NewContext(r)
