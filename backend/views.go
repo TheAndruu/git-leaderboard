4bae8fb0386bfefb29d5454351d4d08cdd3b5913
Andrew Cuga <andycuga@gmail.com>
2018-09-01 11:24:39 -0400

    Adding pageData struct with title and containing array of repo stats

--- backend/views.go
+++ backend/views.go
@@ -3,9 +3,31 @@ package backend
 import (
 	"net/http"
 
+	"github.com/TheAndruu/git-leaderboard/models"
 	"google.golang.org/appengine"
 	"google.golang.org/appengine/log"
 )
+
+// RepoStatsPage has a title, subtitle, and list of RepoStats for display
+type RepoStatsPage struct {
+	Title     string
+	SubTitle  string
+	RepoStats *[]models.RepoStats
+}
+
+// A Welcome message with title, demonstrates passing data to a template
+type Welcome struct {
+	Title   string
+	Message string
+}
+
+// A template taking a struct pointer (&message) containing data to render
+func welcomeHandler(w http.ResponseWriter, r *http.Request) {
+	message := Welcome{Title: "Bootstrap, Go, and GAE", Message: "Bootstrap added to Golang on App Engine.  Feel free to customize further"}
+
+	// outerTheme refernces the template defined within theme.html
+	templates["welcome.html"].ExecuteTemplate(w, "outerTheme", &message)
+}
 
 // Shows the leaders in the current git repos
 func showLeaders(w http.ResponseWriter, r *http.Request) {
@@ -13,9 +35,13 @@ func showLeaders(w http.ResponseWriter, r *http.Request) {
 
 	// get the 10 most recent RepoStats
 	recentStats := GetRecentRepoStats(ctx, 10)
-
 	log.Infof(ctx, "Got the stats %v", len(*recentStats))
 
+	pageData := RepoStatsPage{
+		Title:     "Latest submissions",
+		SubTitle:  "Top committers in the most recently-submitted repositories",
+		RepoStats: recentStats}
+
 	// outerTheme refernces the template defined within theme.html
-	templates["leaderboard.html"].ExecuteTemplate(w, "outerTheme", &recentStats)
+	templates["leaderboard.html"].ExecuteTemplate(w, "outerTheme", pageData)
 }
